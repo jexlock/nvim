42156acadd91a31eb521936910867ad1e2af0c73
polydud <92417496+polydud@users.noreply.github.com>
2023-06-17 18:00:07 +0930

    chore: changed max line guide and removed font

--- lua/polydud/set.lua
+++ lua/polydud/set.lua
@@ -26,7 +26,7 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "100"
 
 vim.g.mapleader = " "
 
@@ -40,4 +40,3 @@ vim.opt.spelllang = "en_au"
 vim.opt.laststatus = 2
 
 vim.opt.encoding = "UTF-8"
-vim.opt.guifont= "Hack"
